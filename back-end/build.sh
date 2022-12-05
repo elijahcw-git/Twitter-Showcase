d411be65f871f62d3ce15cd34669fb87fc9160b0
elijahcw-git <walkerelijahc@gmail.com>
2022-12-04 19:53:25 -0600

    fixed error

--- back-end/build.sh
+++ back-end/build.sh
@@ -6,5 +6,5 @@ npm i --prefix ./../front-end
 npm run build --prefix ./../front-end
 mv ./../front-end/build ./
 
-pip --install --upgrade pip
+pip install --upgrade pip
 pip install -r requirements.txt
